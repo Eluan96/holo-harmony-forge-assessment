07060660d12e7a84a1d4777482d1e1a5b4879e58
Eluan96 <eluan.ea@gmail.com>
2025-11-19 09:50:04 +0100

    final commit

--- holo-harmony-forge-assessment/components/Navbar.tsx
+++ holo-harmony-forge-assessment/components/Navbar.tsx
@@ -90,7 +90,7 @@ const Navbar = () => {
                 Sign In
               </Link>
               <Link
-                href="/get-started"
+                href="/survey"
                 className="bg-[#002E5C] text-white px-4 py-2 rounded-lg text-sm font-medium hover:bg-[#00429E]"
               >
                 Get Started
